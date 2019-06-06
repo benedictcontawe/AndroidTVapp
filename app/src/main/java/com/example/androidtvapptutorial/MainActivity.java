826351372797ddeb30ec406cdaf7e010c2e61f7c
benedict.b.contawe <benedictcontawe@gmail.com>
2019-06-06 09:40:31 +0800

    Android Service using messenger (Getting Random numbers from 0 to 100)

--- app/src/main/java/com/example/androidtvapptutorial/MainActivity.java
+++ app/src/main/java/com/example/androidtvapptutorial/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.androidtvapptutorial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate");
+        Toast.makeText(this,"Hellow",Toast.LENGTH_SHORT).show();
+        serviceIntent = new Intent(getApplicationContext(),MessengerService.class);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startService(serviceIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+        stopService(serviceIntent);
     }
 }
