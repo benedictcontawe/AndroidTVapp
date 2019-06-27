c415c9404b22635dd0ee93c34e2cc47625350f88
benedict.b.contawe <benedictcontawe@gmail.com>
2019-06-27 18:23:00 +0800

    Recalibrate image code

--- app/src/main/java/com/example/androidtvapptutorial/Presenter/MediaVideoPresenter.java
+++ app/src/main/java/com/example/androidtvapptutorial/Presenter/MediaVideoPresenter.java
@@ -30,7 +30,7 @@ public class MediaVideoPresenter extends AbstractCardPresenter<ImageCardView> {
         cardView.setContentText(videoModel.getDescription());
         Glide.with(getContext())
                 .asBitmap()
-                .load(R.drawable.moviex)
+                .load(R.drawable.ic_arrow_white)
                 .into(cardView.getMainImageView());
     }
 }
